1518271a6d35a986d1cf320ad50f9ab2ebf04f6f
KamekuraMaisuke <taisukeoono350@gmail.com>
2021-10-09 22:28:36 +0900

    chap4完成

--- chap4/index.js
+++ chap4/index.js
@@ -2,19 +2,26 @@ import Koa from 'koa'
 import assets from 'koa-static'
 import { Server } from 'socket.io'
 
+const PORT = 3000 || process.env.PORT;
+
 // init koa
 const app = new Koa();
 app.use(assets('public'));
 
 // init server
-const server = app.listen(3000);
+const server = app.listen(PORT);
 
 // init io
 const io = new Server(server);
 
+// クライアントが接続した時の処理
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('接続しました');
+  // 全ての接続しているユーザーに送信するメッセージ
+  socket.emit('message','こんにちは')
+  // 単一の接続しているユーザーに送信するメッセージ
+  socket.broadcast.emit('message','００さんが参加しました。')
   socket.on('disconnect',() => {
-    console.log('user disconnected')
+    console.log('切断しました。')
   })
 });
